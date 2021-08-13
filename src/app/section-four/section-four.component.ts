04837ada64f8fd5e0b96b365092c103633a1ce06
muzammalrahim <creative.joomdev@gmail.com>
2021-08-13 19:41:09 +0500

    Part -4 Button Logic done

--- src/app/section-four/section-four.component.ts
+++ src/app/section-four/section-four.component.ts
@@ -6,23 +6,9 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./section-four.component.css']
 })
 export class SectionFourComponent implements OnInit {
-  showbtn = true;
-  scroll = false;
-  timeout: number;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-  @HostListener('window:scroll', ['$event'])
-  onScroll(e) {
-    var btn = document.getElementById('stickBtn');
-    this.showbtn = false;
-    clearTimeout(this.timeout);
-    setTimeout(() => {
-      this.showbtn = true;
-      btn.classList.toggle('active')
-      console.log('btn', btn.classList);
-    }, 800);
   }
 }
