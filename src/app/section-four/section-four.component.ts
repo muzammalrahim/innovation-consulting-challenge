761d8c63726a65f936bc32a7b3984890697d2b7e
muzammalrahim <creative.joomdev@gmail.com>
2021-08-13 19:09:15 +0500

    stick btn

--- src/app/section-four/section-four.component.ts
+++ src/app/section-four/section-four.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-four',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-four.component.css']
 })
 export class SectionFourComponent implements OnInit {
+  showbtn = true;
+  scroll = false;
+  timeout: number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  @HostListener('window:scroll', ['$event'])
+  onScroll(e) {
+    var btn = document.getElementById('stickBtn');
+    this.showbtn = false;
+    clearTimeout(this.timeout);
+    setTimeout(() => {
+      this.showbtn = true;
+      btn.classList.toggle('active')
+      console.log('btn', btn.classList);
+    }, 800);
+  }
 }
